1bab752472f23bda2291216d876b09c8cc6bc84a
ci-gitlab <tds-developer@xd.com>
2023-10-23 15:18:28 +0800

    feat:update upm

---
+++ Runtime/Public/TapUpdate.cs
@@ -0,0 +1,19 @@
+﻿using TapTap.Update.Internal;
+using TapTap.Common.Internal.Utils;
+using TapTap.Common;
+using System;
+
+namespace TapTap.Update {
+    public class TapUpdate {
+        static readonly ITapUpdateBridge update;
+
+        static TapUpdate() {
+            update = BridgeUtils.CreateBridgeImplementation(typeof(ITapUpdateBridge), "TapTap.Update")
+                as ITapUpdateBridge;
+        }
+
+        public static void UpdateGame(Action onCancel) {
+            update.UpdateGame(onCancel);
+        }
+    }
+}
